1e532e710c173200d6e7e7311ebde737b6341131
akashjpal <palgayatrij786@gmail.com>
2024-06-02 21:19:15 +0530

    Remove error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,31 +4,32 @@ import { useEffect, useState } from "react";
 import Offline from "@/components/Offline";
 
 export default function Home() {
-  const [internet, setInternet] = useState(navigator.onLine);
+  const [internet, setInternet] = useState(true);
 
   useEffect(() => {
-    const updateOnlineStatus = () => {
+    if (typeof window !== "undefined") {
+      const updateOnlineStatus = () => {
+        setInternet(navigator.onLine);
+      };
+
+      // Set the initial status
       setInternet(navigator.onLine);
-    };
 
-    // Set up event listeners for online and offline events
-    window.addEventListener('online', updateOnlineStatus);
-    window.addEventListener('offline', updateOnlineStatus);
+      // Set up event listeners for online and offline events
+      window.addEventListener("online", updateOnlineStatus);
+      window.addEventListener("offline", updateOnlineStatus);
 
-    // Check for internet connection status every 5 seconds
-    const interval = setInterval(updateOnlineStatus, 2000);
+      // Check for internet connection status every 2 seconds
+      const interval = setInterval(updateOnlineStatus, 2000);
 
-    // Clean up event listeners and interval on component unmount
-    return () => {
-      window.removeEventListener('online', updateOnlineStatus);
-      window.removeEventListener('offline', updateOnlineStatus);
-      clearInterval(interval);
-    };
+      // Clean up event listeners and interval on component unmount
+      return () => {
+        window.removeEventListener("online", updateOnlineStatus);
+        window.removeEventListener("offline", updateOnlineStatus);
+        clearInterval(interval);
+      };
+    }
   }, []);
 
-  return (
-    <>
-      {internet ? <App /> : <Offline />}
-    </>
-  );
+  return <>{internet ? <App /> : <Offline />}</>;
 }
